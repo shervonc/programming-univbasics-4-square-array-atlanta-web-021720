7269194dc0aa3ccd0e32da933cf2db73f00b2d5a
Shervon Ceromain <shervonc@gmail.com>
2020-01-31 23:51:52 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -1,7 +1,8 @@
 def square_array(array)
   counter = 0
   while counter < array.length do
-     array[counter]**2
+     newarray = []
+  newarray = array(array[counter]**2).unshift
         counter += 1
   end
 end
